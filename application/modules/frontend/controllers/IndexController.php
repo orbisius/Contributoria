8aea4a5d9b3a82b4c7bc08006036dfc58c5efa04
Daniel Levitt <daniel.levitt@guardian.co.uk>
2012-07-17 14:27:17 +0100

    Added skeleton framework

---
+++ application/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Default entry point in the application
+ *
+ * @package frontend_controllers
+ * @copyright company
+ */
+
+class IndexController extends App_Frontend_Controller
+{
+    /**
+     * Overrides Zend_Controller_Action::init()
+     *
+     * @access public
+     * @return void
+     */
+    public function init()
+    {
+        // init the parent
+        parent::init();
+        
+        $this->_addCommand(new App_Command_SendEmail());
+    }
+    
+    /**
+     * Controller's entry point
+     *
+     * @access public
+     * @return void
+     */
+    public function indexAction()
+    {
+        App_Logger::log('test');
+    }
+    
+    /**
+     * Static Pages
+     *
+     * @return void
+     */
+    public function staticAction()
+    {
+        $page = $this->getRequest()->getParam('page');
+        
+        if (empty($page)) {
+            throw new Exception('Invalid static page identifier');
+        } else {
+            $this->render($page);
+        }
+    }
+}
